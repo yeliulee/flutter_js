790385652616714ba8e6a355d7db3150b784a7d5
yeliulee <yeliuleet@gmail.com>
2024-07-16 12:35:36 +0800

    fix: failed to load fetchPolyfill asset

--- lib/extensions/fetch.dart
+++ lib/extensions/fetch.dart
@@ -11,8 +11,8 @@ extension JavascriptRuntimeFetchExtension on JavascriptRuntime {
     debug('Before enable xhr');
     enableXhr();
     debug('After enable xhr');
-    final fetchPolyfill =
-        await rootBundle.loadString('packages/flutter_js/assets/js/fetch.js');
+    final fetchPolyfill = await rootBundle
+        .loadString('packages/flutter_js_ohos/assets/js/fetch.js');
     debug('Loaded fetchPolyfill');
     final evalFetchResult = evaluate(fetchPolyfill);
     debug('Eval Fetch Result: $evalFetchResult');
